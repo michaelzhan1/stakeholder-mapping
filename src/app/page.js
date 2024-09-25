42dcfc73c358fc9bf42858e81fdc0c82260631e2
Michael Zhan <michaelzhan2001@gmail.com>
2024-09-25 13:45:29 -0400

    Make component ready to test OpenAI querying.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
+import SampleInputAndResponse from "@/components/SampleInputAndResponse";
+
+
 export default function Home() {
   return (
-    <div>
-      Hello, world!
-    </div>
+    <>
+      <div>
+        Hello, world!
+      </div>
+      <SampleInputAndResponse className="mt-3" />
+    </>
   );
 }
