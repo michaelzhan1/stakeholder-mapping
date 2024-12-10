f3d6387f484fd43be4efcd42403e6fb2b40f71ad
Michael Zhan <michaelzhan2001@gmail.com>
2024-12-10 17:06:26 -0500

    Try and fix cors

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, abort, send_file
-from flask_cors import CORS, cross_origin
+from flask import Flask, request, abort, send_file, make_response
+from flask_cors import CORS
 from dotenv import load_dotenv
 from train import run
 import pandas as pd
@@ -12,10 +12,9 @@ if os.getenv('FRONTEND_URL') is None:
     raise ValueError('FRONTEND_URL environment variable must be set in a .env file')
 
 app = Flask(__name__)
-CORS(app, origins=[os.getenv('FRONTEND_URL')])
+CORS(app, origins=['*'])
 
 @app.route('/api/rl-endpoint', methods=['POST'])
-@cross_origin(origin=os.getenv('FRONTEND_URL'))
 def run_rl():
     if request.content_type != 'text/plain':
         abort(400, description='Content-Type must be text/plain')
@@ -31,14 +30,17 @@ def run_rl():
         names = None
         data = full_data
     output = run(data, names, save=True)
+    response = make_response(output)
+    response.headers['Access-Control-Allow-Origin'] = '*'
     return output
 
 @app.route('/api/rl-endpoint/gif', methods=['POST'])
-@cross_origin(origin=os.getenv('FRONTEND_URL'))
 def get_rl_gif():
     fname = 'stakeholder_network.gif'
 
     if not os.path.exists(fname):
         abort(404, description='GIF not found')
     
-    return send_file(fname, mimetype='image/gif')
+    response = send_file(fname, mimetype='image/gif')
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
